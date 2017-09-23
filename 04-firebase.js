74c002e4b29a4bd4c67b6163b727fc1e8fecf536
alumno <alumno@Macbook04.local>
2017-09-23 15:32:48 -0500

    Clase04

--- 04-firebase.js
+++ 04-firebase.js
@@ -2,13 +2,13 @@
 	d.addEventListener('DOMContentLoaded', () => {
 		// Initialize Firebase
 		var config = {
-			apiKey: "AIzaSyCJjTyVmf2de_Au0q7izPEAd5I2ykSbKDY",
-			authDomain: "inventario-76e60.firebaseapp.com",
-			databaseURL: "https://inventario-76e60.firebaseio.com",
-			projectId: "inventario-76e60",
-			storageBucket: "inventario-76e60.appspot.com",
-			messagingSenderId: "346250501510"
-		};
+			apiKey: "AIzaSyDdzVE1lr673KIfOqh9Y2P7dZhR4Y6NCpM",
+			authDomain: "prueba-385b0.firebaseapp.com",
+			databaseURL: "https://prueba-385b0.firebaseio.com",
+			projectId: "prueba-385b0",
+			storageBucket: "",
+			messagingSenderId: "281079538204"
+		  };
 		f.initializeApp(config);
 		
 		const inputCorreo = d.querySelector('#txtCorreo')
@@ -16,5 +16,35 @@
 		const botonLogin = d.querySelector('#btnLogin')
 		const botonRegistro = d.querySelector('#btnRegistro')
 
+		botonRegistro.addEventListener('click', function(e) {
+			e.preventDefault()
+
+			const correo = inputCorreo.value
+			const clave = inputClave.value
+			console.log(correo)
+			console.log(clave)
+			f.auth().createUserWithEmailAndPassword(correo, clave)
+			.then(() => {
+				console.log('usuario registrado')
+			})
+			.catch(error => {
+				console.log(error)
+			})
+		})
+
+		botonLogin.addEventListener('click', function(e) {
+			e.preventDefault()
+
+			const correo = inputCorreo.value
+			const clave = inputClave.value
+
+			f.auth().signInWithEmailAndPassword(correo, clave)
+			.then(() => {
+				console.log('logueado')
+			})
+			.catch(error => {
+				console.log(error)
+			})
+		})
 	})
 })(document, firebase)
